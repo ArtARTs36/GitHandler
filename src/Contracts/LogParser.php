77cc4b7866a4752bf00a4af855ac83da45a5039c
ArtARTs36 <temicska99@mail.ru>
2021-03-30 22:15:22 +0300

    use str in logger

--- src/Contracts/LogParser.php
+++ src/Contracts/LogParser.php
@@ -3,8 +3,9 @@
 namespace ArtARTs36\GitHandler\Contracts;
 
 use ArtARTs36\GitHandler\Data\LogCollection;
+use ArtARTs36\Str\Str;
 
 interface LogParser
 {
-    public function parse(string $raw): ?LogCollection;
+    public function parse(Str $raw): ?LogCollection;
 }
