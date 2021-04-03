d847c60cfbd780cd367fa70cee0b56ccfa87bf55
ArtARTs36 <temicska99@mail.ru>
2021-04-04 01:23:51 +0300

    add client test

---
+++ tests/Unit/SimpleHttpClientTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace ArtARTs36\GitHandler\Tests\Unit;
+
+use ArtARTs36\GitHandler\Support\SimpleHttpClient;
+use GuzzleHttp\Psr7\Request;
+
+class SimpleHttpClientTest extends TestCase
+{
+    /**
+     * @covers \ArtARTs36\GitHandler\Support\SimpleHttpClient::sendRequest
+     */
+    public function testSendRequest(): void
+    {
+        // php://filter/read=string.toupper/resource=data:,dGVzdA==CRAP
+
+        $client = new SimpleHttpClient();
+
+        //
+
+        $response = $client->sendRequest(
+            new Request('GET', 'php://filter/read=string.toupper/resource=data:,')
+        );
+
+        self::assertEmpty($response->getBody()->getContents());
+        self::assertEquals(404, $response->getStatusCode());
+
+        //
+
+        $response = $client->sendRequest(
+            new Request('GET', 'php://filter/read=string.toupper/resource=data:,hello')
+        );
+
+        self::assertEquals('HELLO', $response->getBody()->getContents());
+        self::assertEquals(200, $response->getStatusCode());
+    }
+}
