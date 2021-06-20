f1c4b1de9f1efaf355c90c97b6477b86a4a8140b
ArtARTs36 <temicska99@mail.ru>
2021-06-21 01:21:10 +0300

    add test

--- tests/Unit/RepositoryDownloaderTest.php
+++ tests/Unit/RepositoryDownloaderTest.php
@@ -6,6 +6,7 @@ use ArtARTs36\GitHandler\GitSimpleFactory;
 use ArtARTs36\GitHandler\RepositoryDownloader;
 use ArtARTs36\GitHandler\Tests\Support\ArrayFileSystem;
 use ArtARTs36\GitHandler\Tests\Support\MockHttpClient;
+use Psr\Http\Message\RequestInterface;
 
 class RepositoryDownloaderTest extends TestCase
 {
@@ -45,5 +46,29 @@ class RepositoryDownloaderTest extends TestCase
         );
 
         self::assertEquals('test-file', $result);
+    }
+
+    /**
+     * @covers \ArtARTs36\GitHandler\RepositoryDownloader::createRequestOnFetch
+     */
+    public function testCreateRequestOnFetch(): void
+    {
+        $downloader = new RepositoryDownloader(
+            GitSimpleFactory::factoryOriginUrlSelector(),
+            MockHttpClient::good('test-file'),
+            new ArrayFileSystem()
+        );
+
+        /** @var RequestInterface $result */
+        $result = $this->callMethodFromObject(
+            $downloader,
+            'createRequestOnFetch',
+            $this->mockHasRemotes('https://github.com/ArtARTs36/GitHandler.git')
+        );
+
+        self::assertEquals(
+            (string) $result->getUri(),
+            'https://codeload.github.com/ArtARTs36/GitHandler/zip/refs/heads/master'
+        );
     }
 }
