d5c8d6a585f68fdb6db8af10b6c078c3abc9bd54
ArtARTs36 <temicska99@mail.ru>
2021-06-21 23:53:38 +0300

    add test

---
+++ tests/Unit/Exceptions/PathAlreadyExistsTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace ArtARTs36\GitHandler\Tests\Unit\Exceptions;
+
+use ArtARTs36\GitHandler\Exceptions\PathAlreadyExists;
+use ArtARTs36\GitHandler\Tests\Unit\TestCase;
+use ArtARTs36\Str\Str;
+
+class PathAlreadyExistsTest extends TestCase
+{
+    /**
+     * @covers \ArtARTs36\GitHandler\Exceptions\PathAlreadyExists::__construct
+     * @covers \ArtARTs36\GitHandler\Exceptions\PathAlreadyExists::getMessage
+     */
+    public function testConstructor(): void
+    {
+        $exception = new PathAlreadyExists('/path/to/file');
+
+        self::assertEquals('/path/to/file', $exception->errorPath);
+        self::assertEquals("Path '/path/to/file' already exists", $exception->getMessage());
+    }
+
+    /**
+     * @covers \ArtARTs36\GitHandler\Exceptions\PathAlreadyExists::handleIfSo
+     */
+    public function testHandleIfSo(): void
+    {
+        PathAlreadyExists::handleIfSo('/path/to/file', Str::fromEmpty());
+
+        self::expectException(PathAlreadyExists::class);
+
+        PathAlreadyExists::handleIfSo(
+            '/path/to/file',
+            Str::make("destination path '/path/to/file' already exists")
+        );
+    }
+}
