eaf83a6500b95732903b1d2c591d8c145d805f24
ArtARTs36 <temicska99@mail.ru>
2021-06-21 23:30:27 +0300

    add test

---
+++ tests/Unit/Exceptions/BranchDoesNotHaveCommitsTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ArtARTs36\GitHandler\Tests\Unit\Exceptions;
+
+use ArtARTs36\GitHandler\Exceptions\BranchDoesNotHaveCommits;
+use ArtARTs36\GitHandler\Tests\Unit\TestCase;
+
+class BranchDoesNotHaveCommitsTest extends TestCase
+{
+    /**
+     * @covers \ArtARTs36\GitHandler\Exceptions\BranchHasNoUpstream::__construct
+     */
+    public function testConstructor(): void
+    {
+        $exception = new BranchDoesNotHaveCommits('master');
+
+        self::assertEquals('master', $exception->errorBranch);
+        self::assertEquals("branch 'master' does not have any commits yet", $exception->getMessage());
+    }
+}
