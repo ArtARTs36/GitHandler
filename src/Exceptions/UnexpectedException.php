1d5c6b3a05bb791c043db9874d1dc0ddd065eeb3
ArtARTs36 <temicska99@mail.ru>
2021-06-22 14:46:09 +0300

    add test

--- src/Exceptions/UnexpectedException.php
+++ src/Exceptions/UnexpectedException.php
@@ -6,8 +6,12 @@ use Throwable;
 
 class UnexpectedException extends GitHandlerException
 {
+    public $errorCommand;
+
     public function __construct(string $command, $code = 0, Throwable $previous = null)
     {
+        $this->errorCommand = $command;
+
         $message = "Unexpected exception after execution command: ". $command;
 
         parent::__construct($message, $code, $previous);
