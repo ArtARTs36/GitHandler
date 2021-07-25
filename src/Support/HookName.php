d312692c6b866dcb704ae9eb2c855eec646bbd4b
ArtARTs36 <temicska99@mail.ru>
2021-07-26 01:44:08 +0300

    add hook

--- src/Support/HookName.php
+++ src/Support/HookName.php
@@ -6,5 +6,7 @@ class HookName
 {
     use Enumerable;
 
-    public const PRE_COMMIT_SAMPLE = 'pre-commit.sample'; // before commit
+    public const APPLY_PATH_MSG_SAMPLE = 'applypatch-msg.sample'; // git am
+    public const POST_UPDATE_SAMPLE = 'post-update.sample'; // git push: after push
+    public const PRE_COMMIT_SAMPLE = 'pre-commit.sample'; // git commit: before commit
 }
