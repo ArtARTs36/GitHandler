18cfd376c22dbba55b2c47fbbeadc8ea5ca0a948
ArtARTs36 <temicska99@mail.ru>
2021-08-02 17:05:30 +0300

    clean

--- src/Config/Subjects/Branch.php
+++ src/Config/Subjects/Branch.php
@@ -2,14 +2,21 @@
 
 namespace ArtARTs36\GitHandler\Config\Subjects;
 
+use ArtARTs36\GitHandler\Support\ToArray;
+
 class Branch
 {
+    use ToArray;
+
     public $name;
 
     public $remote;
 
     public $merge;
 
+    /**
+     * @codeCoverageIgnore
+     */
     public function __construct(string $name, string $remote, string $merge)
     {
         $this->name = $name;
