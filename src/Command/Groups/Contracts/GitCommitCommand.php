f08a922b43931211e680e254913ac3a81086647a
ArtARTs36 <temicska99@mail.ru>
2021-08-16 12:34:17 +0300

    add comments

--- src/Command/Groups/Contracts/GitCommitCommand.php
+++ src/Command/Groups/Contracts/GitCommitCommand.php
@@ -2,8 +2,16 @@
 
 namespace ArtARTs36\GitHandler\Command\Groups\Contracts;
 
+/**
+ * Git Commits
+ */
 interface GitCommitCommand
 {
+    /**
+     * Execute commit
+     * @git-command git commit -m="{$message}"
+     * @git-command git commit -m="{$message}" --amend
+     */
     public function commit(string $message, bool $amend = false): bool;
 
     /**
