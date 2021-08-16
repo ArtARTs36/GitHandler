ca2b747c806a22e7a149010e44fd5a74ed250ded
ArtARTs36 <temicska99@mail.ru>
2021-08-17 02:43:51 +0300

    up git status

--- src/Command/Groups/Contracts/GitStatusCommand.php
+++ src/Command/Groups/Contracts/GitStatusCommand.php
@@ -4,26 +4,36 @@ namespace ArtARTs36\GitHandler\Command\Groups\Contracts;
 
 use ArtARTs36\Str\Str;
 
+/**
+ * Git Status
+ */
 interface GitStatusCommand
 {
     /**
-     * equals: git status
+     * Get git status
+     * @git-command git status
      */
     public function status(bool $short = false): Str;
 
+    /**
+     * Check has changes
+     */
     public function hasChanges(): bool;
 
     /**
+     * Get untracked files
      * @return array<string>
      */
     public function getUntrackedFiles(): array;
 
     /**
+     * Get modified files
      * @return array<string>
      */
     public function getModifiedFiles(): array;
 
     /**
+     * Get added files
      * @return array<string>
      */
     public function getAddedFiles(): array;
