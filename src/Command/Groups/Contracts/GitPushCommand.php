0171f4a3e69b187f6eba7f06e6f76e2993618d0e
ArtARTs36 <temicska99@mail.ru>
2021-08-17 02:36:44 +0300

    up git push

--- src/Command/Groups/Contracts/GitPushCommand.php
+++ src/Command/Groups/Contracts/GitPushCommand.php
@@ -4,16 +4,23 @@ namespace ArtARTs36\GitHandler\Command\Groups\Contracts;
 
 use ArtARTs36\GitHandler\Exceptions\BranchHasNoUpstream;
 
+/**
+ * Git Push
+ */
 interface GitPushCommand
 {
     /**
-     * @return bool
+     * Push to remote git
+     * @git-command git push
+     * @git-command git push --force
+     * @git-command git push --force set-upstream $upStream
      * @throws BranchHasNoUpstream
      */
     public function push(bool $force = false, ?string $upStream = null): bool;
 
     /**
-     * equals: git push --set-upstream origin {current-branch}
+     * Push to remote git through current-branch
+     * @git-command git push --set-upstream origin {current-branch}
      */
     public function pushOnAutoSetUpStream(bool $force = false): bool;
 }
