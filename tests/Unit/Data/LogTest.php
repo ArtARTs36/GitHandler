3fca815b79b65c6a69d07e6ab462193d1130243c
ArtARTs36 <temicska99@mail.ru>
2021-08-17 12:27:26 +0300

    remove deprecated test

--- tests/Unit/Data/LogTest.php
+++ tests/Unit/Data/LogTest.php
@@ -10,21 +10,6 @@ use ArtARTs36\GitHandler\Tests\Unit\TestCase;
 class LogTest extends TestCase
 {
     /**
-     * @covers \ArtARTs36\GitHandler\Data\Log::getAbbreviatedCommitHash
-     */
-    public function testGetAbbreviatedCommitHash(): void
-    {
-        $log = new Log(
-            $commit = new Commit('7bfab23737fad677905ae7ddd3ac76e2e31f30a4'),
-            new \DateTime(),
-            new Author('', ''),
-            ''
-        );
-
-        self::assertEquals($commit->getAbbreviatedHash(), $log->getAbbreviatedCommitHash());
-    }
-
-    /**
      * @covers \ArtARTs36\GitHandler\Data\Log::equalsDate
      */
     public function testEqualsDate(): void
