84cc762839d107c484c76b19c123c6409b4742d2
ArtARTs36 <temicska99@mail.ru>
2021-08-17 14:00:46 +0300

    add ResetMode

--- src/Enum/ResetMode.php
+++ src/Enum/ResetMode.php
@@ -7,4 +7,8 @@ use ArtARTs36\GitHandler\Support\Enumerable;
 class ResetMode
 {
     use Enumerable;
+
+    public const SOFT = 'soft';
+    public const HARD = 'hard';
+    public const MIXED = 'mixed';
 }
