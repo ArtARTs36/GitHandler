56a434ef36ea911a72cc9e6afa2fa180e7f80d2b
ArtARTs36 <temicska99@mail.ru>
2021-08-18 22:31:26 +0300

    add test

--- tests/Unit/Commands/ArchiveCommandTest.php
+++ tests/Unit/Commands/ArchiveCommandTest.php
@@ -11,13 +11,23 @@ final class ArchiveCommandTest extends GitTestCase
     /**
      * @covers \ArtARTs36\GitHandler\Command\Commands\ArchiveCommand::create
      */
-    public function testCreate(): void
+    public function testCreateWithPathDir(): void
     {
         $this->mockCommandExecutor->nextFailed('fatal: could not create archive file \'var.zip\': Is a directory');
 
         self::expectException(PathIsDirectoryNotCould::class);
 
         $this->makeArchiveCommand()->create('var.zip');
+    }
+
+    /**
+     * @covers \ArtARTs36\GitHandler\Command\Commands\ArchiveCommand::create
+     */
+    public function testCreateOk(): void
+    {
+        $this->mockCommandExecutor->nextOk();
+
+        self::assertNull($this->makeArchiveCommand()->create('var.zip'));
     }
 
     private function makeArchiveCommand(): ArchiveCommand
