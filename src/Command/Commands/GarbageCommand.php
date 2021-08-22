4c35222ce583b7ec1a9996420cb9e07772416ff2
ArtARTs36 <temicska99@mail.ru>
2021-08-22 03:15:44 +0300

    add test

--- src/Command/Commands/GarbageCommand.php
+++ src/Command/Commands/GarbageCommand.php
@@ -5,20 +5,30 @@ namespace ArtARTs36\GitHandler\Command\Commands;
 use ArtARTs36\GitHandler\Contracts\Commands\GitGarbageCommand;
 use ArtARTs36\GitHandler\Enum\GarbageCollectMode;
 use ArtARTs36\ShellCommand\Interfaces\ShellCommandInterface;
+use ArtARTs36\ShellCommand\Result\CommandResult;
 
 class GarbageCommand extends AbstractCommand implements GitGarbageCommand
 {
-    public function collect(GarbageCollectMode $mode): void
+    public function collect(GarbageCollectMode $mode): bool
     {
-        $this->buildPureCommand($mode)->executeOrFail($this->executor);
+        return $this->analyzeCollectAnswer($this->buildPureCommand($mode)->executeOrFail($this->executor));
     }
 
-    public function collectOnDate(GarbageCollectMode $mode, \DateTimeInterface $date): void
+    public function collectOnDate(GarbageCollectMode $mode, \DateTimeInterface $date): bool
     {
-        $this
+        return $this->analyzeCollectAnswer($this
             ->buildPureCommand($mode)
             ->addOptionWithValue('prune', $date->format('Y-m-d'))
-            ->executeOrFail($this->executor);
+            ->executeOrFail($this->executor));
+    }
+
+    protected function analyzeCollectAnswer(CommandResult $result): bool
+    {
+        if ($result->getResult()->contains('Nothing new to pack.')) {
+            return false;
+        }
+
+        return true;
     }
 
     protected function buildPureCommand(GarbageCollectMode $mode): ShellCommandInterface
--- src/Contracts/Commands/GitGarbageCommand.php
+++ src/Contracts/Commands/GitGarbageCommand.php
@@ -13,11 +13,11 @@ interface GitGarbageCommand
      * Collect garbage
      * @git-command git gc --$mode
      */
-    public function collect(GarbageCollectMode $mode): void;
+    public function collect(GarbageCollectMode $mode): bool;
 
     /**
      * Collect garbage on older date
      * @git-command git gc --$mode --prune=$date
      */
-    public function collectOnDate(GarbageCollectMode $mode, \DateTimeInterface $date): void;
+    public function collectOnDate(GarbageCollectMode $mode, \DateTimeInterface $date): bool;
 }
