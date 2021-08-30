17ce30ae2d35b6074cd886986d248ac476bc585b
ArtARTs36 <temicska99@mail.ru>
2021-08-30 19:51:29 +0300

    add comments

--- src/Contracts/Factory/GitHandlerFactory.php
+++ src/Contracts/Factory/GitHandlerFactory.php
@@ -4,7 +4,14 @@ namespace ArtARTs36\GitHandler\Contracts\Factory;
 
 use ArtARTs36\GitHandler\Contracts\Handler\GitHandler;
 
+/**
+ * Factory for create GitHandler instance
+ */
 interface GitHandlerFactory
 {
+    /**
+     * @param string $dir - path to repository directory
+     * @param string $bin - git binary
+     */
     public function factory(string $dir, string $bin = 'git'): GitHandler;
 }
