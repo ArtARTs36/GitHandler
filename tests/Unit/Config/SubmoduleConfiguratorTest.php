ab2f95ef2a1a9473160bd3fd9c4c34290e3d75aa
ArtARTs36 <temicska99@mail.ru>
2021-09-09 19:49:09 +0300

    add test

---
+++ tests/Unit/Config/SubmoduleConfiguratorTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace ArtARTs36\GitHandler\Tests\Unit\Config;
+
+use ArtARTs36\GitHandler\Config\Configurators\SubmoduleConfigurator;
+use ArtARTs36\GitHandler\Tests\Unit\TestCase;
+
+final class SubmoduleConfiguratorTest extends TestCase
+{
+    public function providerForTestParse(): array
+    {
+        return [
+            [
+                [
+                    'str.url' => 'site.ru',
+                    'str.active' => 'false',
+                ],
+                'str',
+                [
+                    'url' => 'site.ru',
+                    'active' => false,
+                    'name' => 'str',
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider providerForTestParse
+     * @covers \ArtARTs36\GitHandler\Config\Configurators\SubmoduleConfigurator::parse
+     */
+    public function testParse(array $raw, string $key, array $expected): void
+    {
+        $configurator = $this->makeSubmoduleConfigurator();
+
+        $result = $configurator->parse($raw);
+
+        self::assertEquals($expected, $result->get($key)->toArray());
+    }
+
+    private function makeSubmoduleConfigurator(): SubmoduleConfigurator
+    {
+        return new SubmoduleConfigurator();
+    }
+}
