a2cb9e32967e9c7422e191b2c488990c0672abc7
ArtARTs36 <temicska99@mail.ru>
2021-09-11 10:05:25 +0300

    up

---
+++ tests/Unit/Config/GitConfigKeyPropertyMapperTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace ArtARTs36\GitHandler\Tests\Unit\Config;
+
+use ArtARTs36\GitHandler\Config\Mapper\GitConfigKeyPropertyMapper;
+use ArtARTs36\GitHandler\Tests\Unit\Config\Prototypes\Car;
+use ArtARTs36\GitHandler\Tests\Unit\TestCase;
+
+class GitConfigKeyPropertyMapperTest extends TestCase
+{
+    /**
+     * @covers \ArtARTs36\GitHandler\Config\Mapper\GitConfigKeyPropertyMapper::map
+     */
+    public function testCreateObjectFromArray(): void
+    {
+        $mapper = GitConfigKeyPropertyMapper::make();
+
+        $result = $mapper->createObjectFromArray(Car::class, [
+            'self_color' => 'red',
+        ]);
+
+        self::assertEquals([
+            Car::class,
+            'red',
+        ], [get_class($result), $result->color]);
+    }
+}
