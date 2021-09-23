9e7cbe3e5540d629af4803efbfc8548c503c28aa
ArtARTs36 <temicska99@mail.ru>
2021-09-23 18:57:07 +0300

    add comment

--- src/Contracts/Backup/BackupElement.php
+++ src/Contracts/Backup/BackupElement.php
@@ -6,8 +6,14 @@ use ArtARTs36\GitHandler\Contracts\Handler\GitHandler;
 
 interface BackupElement
 {
+    /**
+     * Dump
+     */
     public function dump(GitHandler $git): array;
 
+    /**
+     * Restore dumped data from array
+     */
     public function restore(GitHandler $git, array $data): void;
 
     /**
