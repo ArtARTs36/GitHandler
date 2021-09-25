f7a59b7bb473685a29d3f6e65f7373a8d7699fa3
ArtARTs36 <temicska99@mail.ru>
2021-09-25 16:38:23 +0300

    add attributes to Alias

--- src/Config/Subjects/Alias.php
+++ src/Config/Subjects/Alias.php
@@ -2,10 +2,15 @@
 
 namespace ArtARTs36\GitHandler\Config\Subjects;
 
+use ArtARTs36\GitHandler\Attributes\ConfigKey;
+use ArtARTs36\GitHandler\Attributes\ConfigSectionName;
+
 class Alias extends AbstractSubject
 {
+    #[ConfigSectionName]
     public $name;
 
+    #[ConfigKey()]
     public $script;
 
     public function __construct(string $name, string $script)
