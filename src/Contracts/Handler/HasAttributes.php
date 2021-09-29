6066efc479d8325226e21b0116d74b785b8e9e5b
ArtARTs36 <temicska99@mail.ru>
2021-09-29 12:47:50 +0300

    add comment

--- src/Contracts/Handler/HasAttributes.php
+++ src/Contracts/Handler/HasAttributes.php
@@ -6,5 +6,8 @@ use ArtARTs36\GitHandler\Contracts\Commands\GitAttributeCommand;
 
 interface HasAttributes
 {
+    /**
+     * Git attributes
+     */
     public function attributes(): GitAttributeCommand;
 }
