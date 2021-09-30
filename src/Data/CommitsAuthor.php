46d53e5b07332ba605ce18e6673cd0ded277236e
ArtARTs36 <temicska99@mail.ru>
2021-09-30 12:58:11 +0300

    fix stat-analyse

--- src/Data/CommitsAuthor.php
+++ src/Data/CommitsAuthor.php
@@ -23,7 +23,7 @@ class CommitsAuthor implements \IteratorAggregate, \Countable
     }
 
     /**
-     * @param array<string, Author|array<CommitsAuthor>> $data
+     * @param array<string, Author|array<Commit>> $data
      */
     public static function fromArray(
         #[ArrayShape(['author' => Author::class, 'commits' => 'array'])]
